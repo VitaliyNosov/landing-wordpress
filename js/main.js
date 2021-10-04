5792fdb0a7ca2ea5580dcf67694e31c317bb38db
Vitaliy <vitaliynosov2014@gmail.com>
2021-10-04 17:47:13 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+// burger menu
+
+$('.burger__button').click(function () {
+  $('.menu').slideToggle(500);
+});
+$(window).resize(function () {
+  if ($(window).width() > 500) {
+      $('.menu').removeAttr('style');
+  }
+});
+
+// slider
+
 $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 2,
+    dots: true,
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
   });
+
